f70d4e0b78a2a41c01186b31a514c5a7cbab10b7
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-07-08 21:59:37 -0300

    feat: add create question, create room and get room questions

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,9 @@ import {
 } from 'fastify-type-provider-zod';
 import { sql } from './db/connection.ts';
 import { env } from './env.ts';
+import { createQuestionRoute } from './https/routes/create-question.ts';
+import { createRoomRoute } from './https/routes/create-room.ts';
+import { getRoomQuestionsRoute } from './https/routes/get-room-questions.ts';
 import { getRoomsRoute } from './https/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -23,5 +26,8 @@ app.get('/health', () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomQuestionsRoute);
+app.register(createQuestionRoute);
 
 app.listen({ port: env.PORT });
