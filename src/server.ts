2e6078ac644350cc21faec3a9c9284d6064378b3
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-07-10 15:08:15 -0300

    feat: server logic to work with gemini + audio chunks

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,25 @@
 import { fastifyCors } from '@fastify/cors';
+import { fastifyMultipart } from '@fastify/multipart';
 import { fastify } from 'fastify';
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
-import { sql } from './db/connection.ts';
 import { env } from './env.ts';
 import { createQuestionRoute } from './https/routes/create-question.ts';
 import { createRoomRoute } from './https/routes/create-room.ts';
 import { getRoomQuestionsRoute } from './https/routes/get-room-questions.ts';
 import { getRoomsRoute } from './https/routes/get-rooms.ts';
+import { uploadAudioRoute } from './https/routes/upload-audio.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 });
+
+app.register(fastifyMultipart);
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -29,5 +32,6 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestionsRoute);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT });
